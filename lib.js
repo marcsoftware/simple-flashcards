71faba81a5038fbef95e572dbc9dd99f090c124f
marcsoftware <marcdavidmelcher@gmail.com>
2020-11-29 15:03:36 -0600

    radio options are now shuffled

--- lib.js
+++ lib.js
@@ -44,3 +44,16 @@ function compare(left,right){
     right=right.toLowerCase().trim();
     return left === right;
 }
+
+//-----------------------------------------------------------------------------
+//
+//-----------------------------------------------------------------------------
+/* Randomize array in-place using Durstenfeld shuffle algorithm */
+function shuffle(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
