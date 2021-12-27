349afa70116b69c6591f1a1eb65d9df4dea51221
marcsoftware <marcdavidmelcher@gmail.com>
2021-12-27 12:09:15 -0600

    bugfix: if cookie dont exsists assume zero

--- cookieLibrary.js
+++ cookieLibrary.js
@@ -13,8 +13,12 @@ function setCookie(name,value) {
 //-----------------------------------------------------------------------
 function getCookie(name) {
 
-
-     var p = localStorage.getItem(name); 
+var p=0;
+     try{
+      p = localStorage.getItem(name); 
+   }catch(e){
+      p=0;
+   }
    p=parseInt(p)
    
 if(isNaN(p)){
